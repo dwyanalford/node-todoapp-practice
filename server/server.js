ee4dbf7fff03e4f2840bb210fb91d077834c1db8
dwyanalford <dwyan.alford@gmail.com>
2017-08-19 22:19:24 -0400

    finding :id in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // root of app
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { db } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -30,7 +31,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then( (todo) => {
+  if (!todo) {
+    return res.status(400).send();
+  }
+  res.send({ todo });
+}).catch( (e) => {
+  res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
