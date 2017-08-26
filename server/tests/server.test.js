8b8d71f63360e968f2413403b7b22188882cc942
dwyanalford <dwyan.alford@gmail.com>
2017-08-26 16:10:15 -0400

    add tests for DELETE users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -252,4 +252,25 @@ describe('POST /users/login', () => {
         }).catch((e) => done());
       });
   });
-})
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // we need seed data to get this done
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        // now querying the database here with mongoose
+        // to see if
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done());
+      });
+  });
+});
